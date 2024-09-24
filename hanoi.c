fe7690ff7c71688c161e74ddbf4490a4535e6f01
Nicolarius Shulhin <kolyashulgin98@gmail.com>
2024-09-24 19:15:46 +0200

    fix hanoi

--- hanoi.c
+++ hanoi.c
@@ -4,11 +4,11 @@ void hanoi(int n, int a, int b)
 {
   if (n == 1) 
     printf("move stone 1 from %d to %d\n", a, b);
-  else
+  else 
   {
-    hanoi(n - 1, a, 6 - a);
+    hanoi(n - 1, a, 6 - a - b);
     printf("move stone %d from %d to %d\n", n, a, b);
-    hanoi(n, 6 - a - b, b);
+    hanoi(n -1, 6 - a - b, b);
   }
 }
 
